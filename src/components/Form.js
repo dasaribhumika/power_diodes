08ab43ea912868e62ffb96cc0a37d3130a370d54
Dasari Bhumika <86663780+dasaribhumika@users.noreply.github.com>
2024-03-17 20:42:39 -0400

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const Form = () => {
-  const [piTValue, setPiTValue] = useState('');
+  const [piT, setpiT] = useState('low_power_diodes');
   const [tauIValue, setTauIValue] = useState('');
   const [deltaTValue, setDeltaTValue] = useState('');
   const [tauOnValue, setTauOnValue] = useState('');
@@ -15,16 +15,19 @@ const Form = () => {
   const [lambda0SubcategoryValue, setLambda0SubcategoryValue] = useState('');
   const [lambda0TransientValue, setLambda0TransientValue] = useState('');
   const [lambdaBValue, setLambdaBValue] = useState('');
+  const [tjValue, setTjValue] = useState('');
+  const [missionProfilePhase, setMissionProfilePhase] = useState('');
+  const [temperature, setTemperature] = useState('');
 
   useEffect(() => {
     calculateLambdaValue();
-  }, [piTValue, tauIValue, deltaTValue, tauOnValue, tauOffValue, piUValue, lambdaEOSValue, piIValue, telecomsSubcategoryValue, lambda0Value, lambda0SubcategoryValue, lambda0TransientValue, lambdaBValue]);
-
+  }, [piT, tauIValue, deltaTValue, tauOnValue, tauOffValue, piUValue, lambdaEOSValue, piIValue, telecomsSubcategoryValue, lambda0Value, lambda0SubcategoryValue, lambda0TransientValue, lambdaBValue, tjValue, missionProfilePhase, temperature]);
+ 
   const calculateLambdaValue = () => {
-    if (piTValue && tauIValue && deltaTValue && tauOnValue && tauOffValue && piUValue && lambdaEOSValue && lambdaBValue) {
+    if (piT && tauIValue && deltaTValue && tauOnValue && tauOffValue && piUValue && piIValue && lambdaEOSValue && lambdaBValue && missionProfilePhase && temperature && tjValue) {
       const ΔT = parseFloat(deltaTValue);
       const πn = (ΔT / 3) + 30;
-  
+
       let lambda = 0;
       switch (piUValue) {
         case 'permanent_use':
@@ -39,7 +42,7 @@ const Form = () => {
         default:
           break;
       }
-      
+
       if (lambdaEOSValue === 'protection_interface') {
         switch (piIValue) {
           case 'computer':
@@ -75,7 +78,7 @@ const Form = () => {
       } else if (lambdaEOSValue === 'non_interface') {
         lambda = 0;
       }
-  
+
       // Adjust lambda based on lambdaBValue
       switch (lambdaBValue) {
         case 'TO-18':
@@ -104,10 +107,10 @@ const Form = () => {
           break;
         case 'SOT-346':
           lambda += 1;
-          break;  
+          break;
         case 'SOT-363':
           lambda += 0.8;
-          break;  
+          break;
         case 'SOT-457':
           lambda += 1.1;
           break;
@@ -195,19 +198,22 @@ const Form = () => {
         case 'SOD-15':
           lambda += 5.1;
           break;
-          
+
         default:
           break;
       }
-  
       const result = (lambda * πn * Math.pow(ΔT, 0.68)) * 1e-9;
-  
+
       setLambdaValue(result.toString());
     } else {
       setLambdaValue('');
     }
   };
-  
+
+  const DEFAULT_PI_I_VALUE = 'computer';
+  const DEFAULT_TELECOMS_SUBCATEGORY_VALUE = 'switching';
+  const DEFAULT_LAMBDA0_TRANSIENT_VALUE = 'transient_voltage';
+
 
   const handlePiUChange = (event) => {
     setPiUValue(event.target.value);
@@ -215,17 +221,90 @@ const Form = () => {
 
   const handleLambdaEOSChange = (event) => {
     setLambdaEOSValue(event.target.value);
-    setTelecomsSubcategoryValue(''); // Reset telecomsSubcategory if lambdaEOS changes
+    setPiIValue(DEFAULT_PI_I_VALUE); // Set default value for piI
+    setTelecomsSubcategoryValue('');
   };
+
+  const handleMissionProfilePhaseChange = (event) => {
+    setMissionProfilePhase(event.target.value);
+  };
+
+  const handleTemperatureChange = (event) => {
+    setTemperature(event.target.value);
+  };
+
+  const updateTauIValue = () => {
+    switch (temperature) {
+      case 'Temp 1':
+        if (missionProfilePhase === 'Motor Control') {
+          setTauIValue('0.020');
+        } else if (missionProfilePhase === 'Passenger Compartment') {
+          setTauIValue('0.006');
+        }
+        break;
+      case 'Temp 2':
+        if (missionProfilePhase === 'Motor Control') {
+          setTauIValue('0.015');
+        } else if (missionProfilePhase === 'Passenger Compartment') {
+          setTauIValue('0.046');
+        }
+        break;
+      case 'Temp 3':
+        if (missionProfilePhase === 'Motor Control') {
+          setTauIValue('0.023');
+        } else if (missionProfilePhase === 'Passenger Compartment') {
+          setTauIValue('0.006');
+        }
+        break;
+      default:
+        setTauIValue('');
+        break;
+    }
+  };
+
+  const updateTauOnValue = () => {
+    let tauOn = '';
+    if (missionProfilePhase === 'Motor Control') {
+          tauOn = '0.058';
+    } else if (missionProfilePhase === 'Passenger Compartment') {
+          tauOn = '0.058';
+    }
+    setTauOnValue(tauOn);
+  };
+  
+  const updateTauOffValue = () => {
+    let tauOff = '';
+  
+    if (missionProfilePhase === 'Motor Control') {
+          tauOff = '0.942';
+    } else if (missionProfilePhase === 'Passenger Compartment') {
+          tauOff = '0.942';
+    }
+    setTauOffValue(tauOff);
+  };
+  useEffect(() => {
+  updateTauOnValue();
+  updateTauOffValue();
+}, [missionProfilePhase, updateTauOnValue, updateTauOffValue]);
+
+  // Call updateTauIValue whenever missionProfilePhase or temperature changes
+  useEffect(() => {
+    updateTauIValue();
+  }, [missionProfilePhase, temperature, updateTauIValue]);
 
   const handlePiIChange = (event) => {
-    setPiIValue(event.target.value);
+    const newValue = event.target.value;
+    setPiIValue(newValue !== '' ? newValue : DEFAULT_PI_I_VALUE);
     setTelecomsSubcategoryValue(''); // Reset telecomsSubcategory if piI changes
   };
+  
 
   const handleTelecomsSubcategoryChange = (event) => {
-    setTelecomsSubcategoryValue(event.target.value);
+    const newValue = event.target.value;
+    setTelecomsSubcategoryValue(newValue !== '' ? newValue : DEFAULT_TELECOMS_SUBCATEGORY_VALUE);
   };
+  
+  
 
   const handleLambda0Change = (event) => {
     setLambda0Value(event.target.value);
@@ -235,14 +314,239 @@ const Form = () => {
 
   const handleLambda0SubcategoryChange = (event) => {
     setLambda0SubcategoryValue(event.target.value);
+    setTjValue('');
   };
 
   const handleLambda0TransientChange = (event) => {
-    setLambda0TransientValue(event.target.value);
+    const newValue = event.target.value;
+    setLambda0TransientValue(newValue !== '' ? newValue : DEFAULT_LAMBDA0_TRANSIENT_VALUE);
   };
-
+  
+  
   const handleLambdaBChange = (event) => {
     setLambdaBValue(event.target.value);
+  };
+
+  const handlepiTChange = (event) => {
+    const selectedTj = event.target.value;
+    setTjValue('');
+    let piT;
+  
+    if (
+      [
+        'general_purpose_analog',
+        'switching',
+        'fast_recovery',
+        'power_rectifier',
+        'transient_suppressor',
+      ].includes(lambda0SubcategoryValue)
+    ) {
+      // For the specified subcategories
+      switch (selectedTj) {
+        case '25':
+          piT = 1;
+          break;
+        case '30':
+          piT = 1.2;
+          break;
+        case '35':
+          piT = 1.4;
+          break;
+        case '40':
+          piT = 1.6;
+          break;
+        case '45':
+          piT = 1.9;
+          break;
+        case '50':
+          piT = 2.2;
+          break;
+        case '55':
+          piT = 2.6;
+          break;
+        case '60':
+          piT = 3.0;
+          break;
+        case '65':
+          piT = 3.4;
+          break;
+        case '70':
+          piT = 3.9;
+          break;
+        case '75':
+          piT = 4.4;
+          break;
+        case '80':
+          piT = 5.0;
+          break;
+        case '85':
+          piT = 5.7;
+          break;
+        case '90':
+          piT = 6.4;
+          break;
+        case '95':
+          piT = 7.2;
+          break;
+        case '100':
+          piT = 8.0;
+          break;
+        case '105':
+          piT = 9.0;
+          break;
+        case '110':
+          piT = 10.0;
+          break;
+        case '115':
+          piT = 11.0;
+          break;
+        case '120':
+          piT = 12.0;
+          break;
+        case '125':
+          piT = 14.0;
+          break;
+        case '130':
+          piT = 15.0;
+          break;
+        case '135':
+          piT = 16.0;
+          break;
+        case '140':
+          piT = 18.0;
+          break;
+        case '145':
+          piT = 20.0;
+          break;
+        case '150':
+          piT = 21.0;
+          break;
+        case '155':
+          piT = 23.0;
+          break;
+        case '160':
+          piT = 25.0;
+          break;
+        case '165':
+          piT = 28.0;
+          break;
+        case '170':
+          piT = 30.0;
+          break;
+        case '175':
+          piT = 32.0;
+          break;
+        default:
+          piT = 1; // Default value
+          break;
+      }
+    } else {
+      // For other subcategories
+      switch (selectedTj) {
+        case '25':
+          piT = 1;
+          break;
+        case '30':
+          piT = 1.1;
+          break;
+        case '35':
+          piT = 1.2;
+          break;
+        case '40':
+          piT = 1.4;
+          break;
+        case '45':
+          piT = 1.5;
+          break;
+        case '50':
+          piT = 1.6;
+          break;
+        case '55':
+          piT = 1.8;
+          break;
+        case '60':
+          piT = 2.0;
+          break;
+        case '65':
+          piT = 2.1;
+          break;
+        case '70':
+          piT = 2.3;
+          break;
+        case '75':
+          piT = 2.5;
+          break;
+        case '80':
+          piT = 2.7;
+          break;
+        case '85':
+          piT = 3.0;
+          break;
+        case '90':
+          piT = 3.2;
+          break;
+        case '95':
+          piT = 3.4;
+          break;
+        case '100':
+          piT = 3.7;
+          break;
+        case '105':
+          piT = 3.9;
+          break;
+        case '110':
+          piT = 4.2;
+          break;
+        case '115':
+          piT = 4.5;
+          break;
+        case '120':
+          piT = 4.8;
+          break;
+        case '125':
+          piT = 5.1;
+          break;
+        case '130':
+          piT = 5.4;
+          break;
+        case '135':
+          piT = 5.7;
+          break;
+        case '140':
+          piT = 6.0;
+          break;
+        case '145':
+          piT = 6.4;
+          break;
+        case '150':
+          piT = 6.7;
+          break;
+        case '155':
+          piT = 7.1;
+          break;
+        case '160':
+          piT = 7.5;
+          break;
+        case '165':
+          piT = 7.9;
+          break;
+        case '170':
+          piT = 8.3;
+          break;
+        case '175':
+          piT = 8.7;
+          break;
+        default:
+          piT = 1; // Default value
+          break;
+      }
+    }
+  
+    setpiT(piT);
+  };
+  
+  const handleTjChange = (event) => {
+    setTjValue(event.target.value);
   };
 
   return (
@@ -355,18 +659,126 @@ const Form = () => {
         <option value="SMC(DO-215)">SMC(DO-215)</option>
         <option value="DO-220">DO-220</option>
         <option value="SOD-15">SOD-15</option>
-
       </select>
-      <p>Enter πt value:</p>
-      <input type="text" value={piTValue} onChange={(e) => setPiTValue(e.target.value)} />
-      <p>Enter 𝜏i value:</p>
-      <input type="text" value={tauIValue} onChange={(e) => setTauIValue(e.target.value)} />
+      <p>Select Power Diodes:</p>
+      <select value={piT} onChange={handlepiTChange}>
+        <option value="">Select Power Diodes</option>
+        <option value="low_power_diodes">Low Power Diodes</option>
+        <option value="high_power_diodes">High Power Diodes</option>
+        <option value="low_power_transistor">Low Power Transistor</option>
+        <option value="high_power_transistor">High Power Transistor</option>
+      </select>
+      {piT !== '' && (
+        <>
+          <p>Select Diode Subcategory:</p>
+          <select value={lambda0SubcategoryValue} onChange={handleLambda0SubcategoryChange}>
+            <option value="">Select Subcategory</option>
+            <option value="general_purpose_analog">General Purpose Analog</option>
+            <option value="switching">Switching</option>
+            <option value="fast_recovery">Fast Recovery</option>
+            <option value="power_rectifier">Power Rectifier</option>
+            <option value="transient_suppressor">Transient Suppressor</option>
+            <option value="voltage_regulator">Voltage Regulator</option>
+            <option value="voltage_reference">Voltage Reference</option>
+            <option value="current_regulator">Current Regulator</option>
+          </select>
+          {/* Dropdown for Tj based on selected subcategory */}
+          {['general_purpose_analog', 'switching', 'fast_recovery', 'power_rectifier', 'transient_suppressor'].includes(lambda0SubcategoryValue) && (
+            <>
+              <p>Select Tj:</p>
+              <select value={tjValue} onChange={handleTjChange}>
+                <option value="">Select Tj</option>
+                <option value="25">25</option>
+                <option value="30">30</option>
+                <option value="35">35</option>
+                <option value="40">40</option>
+                <option value="45">45</option>
+                <option value="50">50</option>
+                <option value="55">55</option>
+                <option value="60">60</option>
+                <option value="65">65</option>
+                <option value="70">70</option>
+                <option value="75">75</option>
+                <option value="80">80</option>
+                <option value="85">85</option>
+                <option value="90">90</option>
+                <option value="95">95</option>
+                <option value="100">100</option>
+                <option value="105">105</option>
+                <option value="110">110</option>
+                <option value="115">115</option>
+                <option value="120">120</option>
+                <option value="125">125</option>
+                <option value="130">130</option>
+                <option value="135">135</option>
+                <option value="140">140</option>
+                <option value="145">145</option>
+                <option value="150">150</option>
+                <option value="155">155</option>
+                <option value="160">160</option>
+                <option value="165">165</option>
+                <option value="170">170</option>
+                <option value="175">175</option>
+              </select>
+            </>
+          )}
+          {['voltage_regulator', 'voltage_reference', 'current_regulator'].includes(lambda0SubcategoryValue) && (
+            <>
+              <p>Select Tj:</p>
+              <select value={tjValue} onChange={handleTjChange}>
+                <option value="">Select Tj</option>
+                <option value="25">25</option>
+                <option value="30">30</option>
+                <option value="35">35</option>
+                <option value="40">40</option>
+                <option value="45">45</option>
+                <option value="50">50</option>
+                <option value="55">55</option>
+                <option value="60">60</option>
+                <option value="65">65</option>
+                <option value="70">70</option>
+                <option value="75">75</option>
+                <option value="80">80</option>
+                <option value="85">85</option>
+                <option value="90">90</option>
+                <option value="95">95</option>
+                <option value="100">100</option>
+                <option value="105">105</option>
+                <option value="110">110</option>
+                <option value="115">115</option>
+                <option value="120">120</option>
+                <option value="125">125</option>
+                <option value="130">130</option>
+                <option value="135">135</option>
+                <option value="140">140</option>
+                <option value="145">145</option>
+                <option value="150">150</option>
+                <option value="155">155</option>
+                <option value="160">160</option>
+                <option value="165">165</option>
+                <option value="170">170</option>
+                <option value="175">175</option>
+              </select>
+            </>
+          )}
+        </>
+      )}
+        
       <p>Enter ΔT value:</p>
       <input type="text" value={deltaTValue} onChange={(e) => setDeltaTValue(e.target.value)} />
-      <p>Enter 𝜏on value:</p>
-      <input type="text" value={tauOnValue} onChange={(e) => setTauOnValue(e.target.value)} />
-      <p>Enter 𝜏off value:</p>
-      <input type="text" value={tauOffValue} onChange={(e) => setTauOffValue(e.target.value)} />
+      <p>Select Mission Profile Phase:</p>
+      <select value={missionProfilePhase} onChange={handleMissionProfilePhaseChange}>
+        <option value="">Select Mission Profile Phase</option>
+        <option value="Motor Control">Motor Control</option>
+        <option value="Passenger Compartment">Passenger Compartment</option>
+      </select>
+      <p>Select Temperature:</p>
+      <select value={temperature} onChange={handleTemperatureChange}>
+        <option value="">Select Temperature</option>
+        <option value="Temp 1">Temp 1</option>
+        <option value="Temp 2">Temp 2</option>
+        <option value="Temp 3">Temp 3</option>
+      </select>
       <p>λ value:</p>
       <span style={{ fontStyle: 'oblique', color: 'red' }}>{lambdaValue}</span>
     </div>
