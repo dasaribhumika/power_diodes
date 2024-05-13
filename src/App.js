69f94bcb7c341a3d32886deb19867399599bd7ef
Dasari Bhumika <86663780+dasaribhumika@users.noreply.github.com>
2024-05-13 08:29:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 import Form from './components/Form';
+import Form2 from './components/Form2';
+import Form6 from './components/Form6';
 
 
 function App() {
+  // State to track which page is currently active
+  const [currentPage, setCurrentPage] = useState('form-1');
+
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // Render different pages based on currentPage state
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'form-1':
+        return <Form />;
+      // Add more cases for other pages if needed
+      case 'form-2':
+        return <Form2 />;
+      case 'form-3':
+        return <Form6 />;
+     
+      default:
+        return <div>Page not found</div>;
+    }
+  };
+
   return (
     <div className="App">
-            <div className='card'>
-              <Form />
-            </div>
+      <div className="navbar">
+        <ul>
+          <li onClick={() => handlePageChange('form-1')}>Form-1</li>
+          <li onClick={() => handlePageChange('form-2')}>Form-2</li>
+          <li onClick={() => handlePageChange('form-3')}>Form-3</li>
+        </ul>
+      </div>
+      <div className="body">{renderPage()}</div>
     </div>
   );
 }
